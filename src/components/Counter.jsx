bdbf2822396450665a97e041edb824837202bb2c
RexaTheScaren <thisisrexa@gmail.com>
2023-04-05 18:55:42 +0330

    Add tailwindcss integration

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+
+export default function Counter() {
+	const [count, setCount] = useState(0)
+
+	return (
+			<>
+				<h1>Count: {count}</h1>
+				<button onClick={() => setCount(c => c + 1)}>Increment</button>
+			</>
+		)
+}
