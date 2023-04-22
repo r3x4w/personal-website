9856d6ab876380c4dd58bf6f3051abfd046fc13d
RexaTheScaren <thisisrexa@gmail.com>
2023-04-22 17:12:20 +0330

    Update pages and some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-  ],
+  integrations: [react(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx()]
 });
