b1b0a455095017ab048e6f884941ed87a0a59ff0
RexaTheScaren <thisisrexa@gmail.com>
2023-06-09 13:04:05 +0330

    Remove cfgs

--- src/scripts/footer.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const arrowUpButton = document.querySelector('#arrowUp');
-  arrowUpButton.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
-});
